1ec8f03081df89e9e88fcb5b928b2ee5376858d8
GingerGabriela <gnortiz25@gmail.com>
2019-06-18 17:42:56 +0200

    ggg

---
+++ ENDED/src/FFF/Principal.java
@@ -0,0 +1,20 @@
+package FFF;
+
+public class Principal {
+
+	
+	public void grabarComerciales() {
+		
+	}
+	
+	public void evaluaComercial(String omer) {
+		
+	}
+	public static void main(String[] args) {
+		
+		
+		
+	}
+	
+	
+}
